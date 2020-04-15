92d71735bf5656aff5dae5fc3a29ead3cb202182
Melanie Hallowell <melleehall@gmail.com>
2020-04-14 23:47:45 -0600

    Added backin validateBearerToken function and errorHandler to App

--- src/app.js
+++ src/app.js
@@ -14,10 +14,33 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 app.use(cors())
 app.use(helmet())
 
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    console.log(apiToken)
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
 app.use(bookmarksRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
 })
 
 module.exports = app
