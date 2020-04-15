628060aa61cb45b73327821e5725b51df2885142
Melanie Hallowell <melleehall@gmail.com>
2020-04-14 23:40:56 -0600

    Bookmarks app purely using db instead of JS memory for post and delete requests

--- src/app.js
+++ src/app.js
@@ -1,64 +1,23 @@
 require('dotenv').config()
-const { NODE_ENV } = require('./config')
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const bookmarksRouter = require('./bookmarks/bookmark-router')
-const logger = require('./logger')
-const BookmarksService = require('./bookmarks-service')
+const { NODE_ENV } = require('./config')
+const bookmarksRouter = require('../src/bookmarks/bookmark-router.js')
 
 const app = express()
 
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
-
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     console.log(apiToken)
-//     const authToken = req.get('Authorization')
-
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//         logger.error(`Unauthorized request to path: ${req.path}`);
-//         return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     next()
-// })
+app.use(helmet())
 
 app.use(bookmarksRouter)
 
-app.get('/bookmarks', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    BookmarksService.getAllBookmarks(knexInstance)
-        .then(bookmarks => {
-            res.json(bookmarks)
-        })
-        .catch(next)
-})
-
-app.get('/bookmarks/:id', (req, res, next) => {
-    console.log('bookmark id endpoint requested')
-    const knexInstance = req.app.get('db')
-    BookmarksService.getById(knexInstance, req.params.id)
-        .then(bookmark => {
-            if(!bookmark) {
-                return res.status(404).json({
-                    error: { message: `Bookmark with id ${req.params.id} does not exist.`}
-                })
-            }
-            res.json(bookmark)
-        })
-        .catch(next)
-})
-
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (process.env.NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
 module.exports = app
--- src/bookmarks-service.js
+++ src/bookmarks-service.js
@@ -1,10 +1,29 @@
 const BookmarksService = {
     getAllBookmarks(knex) {
-        return knex.select('*').from('web_bookmarks')
+      return knex.select('*').from('web_bookmarks')
     },
     getById(knex, id) {
-        return knex.from('web_bookmarks').select('*').where('id', id).first()
+      return knex.from('web_bookmarks').select('*').where('id', id).first()
     },
-}
-
-module.exports = BookmarksService
+    insertBookmark(knex, newBookmark) {
+      return knex
+        .insert(newBookmark)
+        .into('web_bookmarks')
+        .returning('*')
+        .then(rows => {
+          return rows[0]
+        })
+    },
+    deleteBookmark(knex, id) {
+      return knex('web_bookmarks')
+        .where({ id })
+        .delete()
+    },
+    updateBookmark(knex, id, newBookmarkFields) {
+      return knex('web_bookmarks')
+        .where({ id })
+        .update(newBookmarkFields)
+    },
+  }
+  
+  module.exports = BookmarksService
